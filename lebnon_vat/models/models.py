5bd1fa48fe28c2d995fcc0bff899b80153823f54
MohammedAlii80 <53688407+MohammedAlii80@users.noreply.github.com>
2023-06-09 22:24:28 +0300

    Add files via upload

--- lebnon_vat/models/models.py
+++ lebnon_vat/models/models.py
@@ -27,7 +27,7 @@ class lebnonvat(models.Model):
 
     def vjvwewe(self):
         for rec in self:
-            rec.new_field=(rec.price_unit * rec.taxes_id.amount /100) +rec.price_unit
+            rec.new_field=(rec.price_unit * rec.taxes_id.amount /100)
 
 
     def subtotal_tax(self):
@@ -188,3 +188,137 @@ class NewModule(models.Model):
     def total_lebnon_vat(self):
         for rec in self:
             rec.totallebnon_vat = rec.amount_taxes + rec.untaxtotal_vat
+
+
+
+class saleorderlinelebnon(models.Model):
+    _inherit = 'sale.order.line'
+
+    new_sub = fields.Monetary(string=" New Subtotal",  required=False,compute="subtotal_test" )
+    new_sub_with_tax = fields.Monetary(string="Subtotal With Tax",  required=False,compute="subtotal_tax" )
+    new_field = fields.Float(string="vvjefbvje",  required=False, compute='vjvwewe' )
+    new_field2 = fields.Float(string="gggyu",  required=False, compute='vjvwewe2' )
+
+    def vjvwewe2(self):
+        for rec in self:
+            rec.new_field2 = rec.new_field*rec.dollar_2day
+
+    def vjvwewe(self):
+        for rec in self:
+            rec.new_field=(rec.price_unit * rec.tax_id.amount /100)
+
+
+    def subtotal_tax(self):
+        for rec in self:
+            if rec.price_unit:
+                rec.new_sub_with_tax = (rec.price_unit * rec.tax_id.amount) + rec.price_unit  *(rec.dollar_2day)
+
+
+    def subtotal_test(self):
+        for rec in self:
+            if rec.price_unit:
+                rec.new_sub= rec.price_unit * rec.dollar_out_bank
+
+
+    def testIss_fun(self):
+        y = self.env['ir.config_parameter'].sudo().get_param('lebnon_vat.dollar_today')
+        return y
+
+    dollar_2day = fields.Monetary(string="Dollar Today Bank",default= testIss_fun)
+
+
+    def doolar_fun(self):
+        dollar = self.env['ir.config_parameter'].sudo().get_param('lebnon_vat.dollar_today_t')
+        return dollar
+    dollar_out_bank = fields.Monetary(string="Dollar Out Bank",default= doolar_fun)
+
+
+class saleorderModule(models.Model):
+    _inherit = 'sale.order'
+    total_vat = fields.Monetary(string="بالليرة",  required=False,compute='shipping_one_r')
+    untaxtotal_vat = fields.Monetary(string="باليرة",  required=False,compute='untaxtotal_amount')
+    totallebnon_vat = fields.Monetary(string="Total",  required=False,compute='total_lebnon_vat')
+
+    totallebnon_vat_new = fields.Monetary(string="Total",  required=False,compute='total_lebnon_vat_new')
+    amount_taxes = fields.Monetary(string=" بالليرة", required=False, compute="usssyy_sjs")
+
+
+    text_amount = fields.Char(string="المبلغ بالليرة", required=False, compute="amount_to_text")
+    text_amount_two = fields.Char(string=" بالليرة", required=False, compute="amount_kwwk")
+    text_amount_three = fields.Char(string=" بالليرة", required=False, compute="amount_schjc")
+    text_amount_klhoh = fields.Char(string=" بالليرة", required=False, compute="amount_to_text_uuu")
+
+    @api.depends('amount_taxes')
+    def amount_to_text_uuu(self):
+        for line in self:
+            lang = self.env.user.lang
+            print(lang)
+            line.text_amount_klhoh = num2words(abs(line.amount_taxes), lang='ar_001')+" " +"ليرة"
+
+    @api.depends('totallebnon_vat')
+    def amount_to_text(self):
+        for line in self:
+            lang = self.env.user.lang
+            print(lang)
+            line.text_amount = num2words(abs(line.totallebnon_vat), lang='ar_001')+ " " +"ليرة"
+
+    @api.depends('total_vat')
+    def amount_kwwk(self):
+        for line in self:
+            lang = self.env.user.lang
+            print(lang)
+            line.text_amount_two = num2words(abs(line.total_vat), lang='ar_001')+ " " +"ليرة"
+
+    @api.depends('untaxtotal_vat')
+    def amount_schjc(self):
+        for line in self:
+            lang = self.env.user.lang
+            print(lang)
+            line.text_amount_three = num2words(abs(line.untaxtotal_vat), lang='ar_001') + " " +"ليرة"
+
+    @api.depends('amount_taxes')
+    def usssyy_sjs(self):
+        for rec in self:
+            if rec.order_line:
+                for line in rec.order_line:
+                       rec.amount_taxes += line.new_field2
+
+            else:
+                rec.total_vat = 0
+
+
+    @api.depends('totallebnon_vat_new')
+    def total_lebnon_vat_new(self):
+        for rec in self:
+            if rec.order_line:
+                for line in rec.order_line:
+                    rec.totallebnon_vat_new += line.new_sub_with_tax
+
+            else:
+                rec.total_vat = 0
+
+    @api.depends('total_vat')
+    def shipping_one_r(self):
+        for rec in self:
+            if rec.order_line:
+                for line in rec.order_line:
+                    rec.total_vat += line.new_sub_with_tax
+
+            else:rec.total_vat=0
+
+    @api.depends('untaxtotal_vat')
+    def untaxtotal_amount(self):
+        for rec in self:
+            if rec.order_line:
+                for line in rec.order_line:
+                    rec.untaxtotal_vat += line.new_sub
+
+            else:
+                rec.untaxtotal_vat = 0
+
+
+    def total_lebnon_vat(self):
+        for rec in self:
+            rec.totallebnon_vat = rec.amount_taxes + rec.untaxtotal_vat
+
+
